9483e6b3dc7e8ec3bdd691d0aad24d249e892280
AbdulazizDXT <105378232+AbdulazizDXT@users.noreply.github.com>
2022-08-10 01:33:56 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ sudo apt install unzip -y
 unzip SuperASS.zip
 rm -rf SuperASS.zip
 cd /home/$USERNAME/Desktop/SuperASS/SuperASS
-wget 
+mkdir PY
+cd PY
+wget https://www.python.org/ftp/python/3.10.6/python-3.10.6.exe
+mv * python-3.9.0.exe.exe
+mv python-3.9.0.exe.exe /home/$USERNAME/Desktop/SuperASS/SuperASS
+rm - rf PY
 nautilus /home/$USERNAME/Desktop/SuperASS/SuperASS
-cat THX
+cat THX 
