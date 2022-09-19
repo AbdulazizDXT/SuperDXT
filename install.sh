2c628863de2acf2518ace7945b6cd9384ab4ea45
AbdulazizDXT <105378232+AbdulazizDXT@users.noreply.github.com>
2022-09-19 17:19:06 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt autoremove
 sudo apt --fix-broken install
-mkdir /home/$USERNAME/Desktop/SuperASS
-cd /home/$USERNAME/Desktop/SuperASS
-wget https://raw.githubusercontent.com/AbdulazizDXT/DXT-Server/main/SuperASS.zip 
-mv * SuperASS.zip
+mkdir /home/$USERNAME/Desktop/SuperDXT
+cd /home/$USERNAME/Desktop/SuperDXT
+wget https://raw.githubusercontent.com/AbdulazizDXT/DXT-Server/main/SuperDXT.zip 
+mv * SuperDXT.zip
 sudo apt install unzip -y
-unzip SuperASS.zip
-rm -rf SuperASS.zip
-cd /home/$USERNAME/Desktop/SuperASS/SuperASS
+unzip SuperDXT.zip
+rm -rf SuperDXT.zip
+cd /home/$USERNAME/Desktop/SuperDXT/SuperDXT
 wget https://www.python.org/ftp/python/3.10.6/python-3.10.6.exe
 mv python-3.10.6.exe python-3.9.0.exe.exe
-mv python-3.9.0.exe.exe SuperASS/
-cd /home/$USERNAME/SuperASS
+mv python-3.9.0.exe.exe SuperDXT/
+cd /home/$USERNAME/SuperDXT
 cat THX
-nautilus /home/$USERNAME/Desktop/SuperASS/SuperASS
+nautilus /home/$USERNAME/Desktop/SuperDXT/SuperDXT
 cat THX
